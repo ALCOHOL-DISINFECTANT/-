7606172844a9b0a52aeff4158e04162bf1a57037
少佐先生 <51908080+ALCOHOL-DISINFECTANT@users.noreply.github.com>
2020-06-10 19:36:46 +0800

    Add files via upload

--- 实验4-1.c
+++ 实验4-1.c
@@ -5,20 +5,20 @@ int main()
 {
 	int first,second,input,num;
 	
-	while(1)//????
+	while(1)//无限做题 
 	{
 		srand(time(NULL));
 		first=rand()%10+1;
-		second=rand()%10+1;//???????
-		printf("%d * %d = ",first,second);
+		second=rand()%10+1;//产生两个随机数 
+		printf("%d × %d = ",first,second);
 		scanf("%d",&input);
-		num=first*second;//????
-		while(input!=num)//????????
+		num=first*second;//两数相乘
+		while(input!=num)//不做对不退出 
 		{
 			printf("Wrong! Please try again.\n");
 			scanf("%d",&input);
 		}
-		printf("Right拢隆\n");
+		printf("Right！\n");
 	}
 }
 
